6da137900509af66a84b910c05fccb02c89a78fe
Myriam <mail@mail.com>
2017-12-13 16:18:11 +0100

    Message message

--- FormationGit/GitFormation2017/src/FormationHello.java
+++ FormationGit/GitFormation2017/src/FormationHello.java
@@ -4,12 +4,8 @@ public class FormationHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-<<<<<<< HEAD
 		System.out.println("Myriam");
-=======
 		System.out.println("Les gens");
-
->>>>>>> branch 'master' of https://github.com/med92/formationGit
 	}
 
 }
