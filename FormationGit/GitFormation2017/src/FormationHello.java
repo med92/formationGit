b73d5b8913ff3fbd6711cc2a4a80614db7ae32ff
mnefzi <mnefzi@mnefzi-adaming.neylingservices.tn>
2017-12-13 16:57:13 +0100

    commit de conflit

--- FormationGit/GitFormation2017/src/FormationHello.java
+++ FormationGit/GitFormation2017/src/FormationHello.java
@@ -3,10 +3,13 @@ public class FormationHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello");
 		System.out.println("Myriam");
 		System.out.println("hello");
-		System.out.println("George");
+		System.out.println("Oscar");
+		System.out.println("Qui suis-je ? Jo ");
+		System.out.println("Les gens");
+		System.out.println("Goerge");
 	}
 
 }
-
