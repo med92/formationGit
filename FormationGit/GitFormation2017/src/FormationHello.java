b48a1da5456f954007dcab07390112f8cc156382
Jonathan <djow.attal@gmail.com>
2017-12-13 16:20:52 +0100

    Test commit 2 Jo

--- FormationGit/GitFormation2017/src/FormationHello.java
+++ FormationGit/GitFormation2017/src/FormationHello.java
@@ -4,7 +4,7 @@ public class FormationHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		System.out.println("Qui suis-je ? Jo");
+		System.out.println("Qui suis-je ? Jo ");
 		System.out.println("Les gens");
 
 	}
